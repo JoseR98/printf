375eb9789e3c1f68e5202ae367b4bfa09b3a98f8
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-03-17 04:52:04 +0000

    Add changes to octal and hex

--- check_percent.c
+++ check_percent.c
@@ -55,6 +55,11 @@ int validate_odd(const char *format, int *i, va_list args, int num_percent)
 		{"c", fn_char},
 		{"d", fn_int},
 		{"i", fn_int},
+		{"b", fn_bin},
+		{"o", fn_octal},
+		{"u", fn_un},
+		{"X", fn_Hexa},
+		{"x", fn_hexa},
 		{NULL, NULL}
 	};
 	int counter = 0, num_chars2 = 0, j;
@@ -97,6 +102,11 @@ int validate_even(const char *format, int *i, va_list args, int num_percent)
 		{"c", fn_char},
 		{"d", fn_int},
 		{"i", fn_int},
+		{"b", fn_bin},
+		{"o", fn_octal},
+		{"u", fn_un},
+		{"X", fn_Hexa},
+		{"x", fn_hexa},
 		{NULL, NULL}
 	};
 	int counter = 0, num_chars2 = 0, j;
